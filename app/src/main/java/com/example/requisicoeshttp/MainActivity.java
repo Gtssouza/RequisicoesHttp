bba50264d1e6ccf49eeaa2e9580656cd4fa7f584
Gustavo Souza <gtssouza94@gmail.com>
2020-08-27 17:20:58 -0300

    Recuperando dados da API

--- app/src/main/java/com/example/requisicoeshttp/MainActivity.java
+++ app/src/main/java/com/example/requisicoeshttp/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.requisicoeshttp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnRecuperar;
@@ -22,8 +29,40 @@ public class MainActivity extends AppCompatActivity {
         btnRecuperar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                MyTask task = new MyTask();
+                String urlApi = "https://blockchain.info/ticker";
+                task.execute(urlApi);
             }
         });
+    }
+    //Retorna a URL,(caso fosse uma barra de load seria um inteiro), retorna uma URL
+    class MyTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String stringUrl = strings[0];
+            //criando conexão com API
+            try {
+                URL url = new URL(stringUrl);
+                HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+
+                InputStream inputStream = conexao.getInputStream();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
